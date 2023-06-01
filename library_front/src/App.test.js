972dadc8e55963665f8a92608410e0791c51a270
Pedro Miranda <pcamposmiranda@gmail.com>
2023-06-01 18:26:21 -0300

    Novos Testes

--- library_front/src/App.test.js
+++ library_front/src/App.test.js
@@ -1,8 +1,46 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { validateEmail, validateName, validateSignUp } from './Utils/validations/index.js';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Email Validation', () => {
+  test('Valid email should return true', () => {
+    const email = 'test@example.com';
+    const result = validateEmail(email);
+    expect(result).toBe(true);
+  });
+
+  test('Invalid email should return false', () => {
+    const email = 'invalid_email';
+    const result = validateEmail(email);
+    expect(result).toBe(false);
+  });
+});
+
+describe('Name Validation', () => {
+  test('Valid name should return true', () => {
+    const name = 'John Doe';
+    const result = validateName(name);
+    expect(result).toBe(true);
+  });
+
+  test('Invalid name should return false', () => {
+    const name = '123';
+    const result = validateName(name);
+    expect(result).toBe(false);
+  });
+});
+
+describe('Sign Up Validation', () => {
+  test('Valid email should return true', () => {
+    const email = 'test@example.com';
+    const name = 'John Doe';
+    const result = validateSignUp(email, name);
+    expect(result).toBe(true);
+  });
+
+  test('Invalid email should return false', () => {
+    const email = 'invalid_email';
+    const name = 'John Doe';
+    const result = validateSignUp(email, name);
+    expect(result).toBe(false);
+  });
 });
